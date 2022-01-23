f0b3e17063977e70906f6a740d71019080beb445
deafcon01 <deafcon01@gmail.com>
2022-01-23 14:18:43 +0530

    Add custom user model

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # recipe-app-api
 Recipe app api source code
+
+#### To execute and start running 
+``` python3
+docker-compose run app sh -c "django-admin.py startproject app ."
+```
+
+#### To execute tests
+```python3
+docker-compose run app sh -c "python manage.py test && flake8"
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,43 @@
+from webbrowser import get
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating user with email"""
+        email = "test@gmail.com"
+        password = "test123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalised(self):
+        """Test the email for a new user is normalzed"""
+        email = "test@GMAIL.COM"
+        user = get_user_model().objects.create_user(
+            email, 'test123'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """test if no email passed"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                None, 'test123'
+            )
+
+    def test_create_new_superuser(self):
+        """"""
+        user = get_user_model().objects.create_user(
+            "test@gmail.com",
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
